759444276bf9cfc36436c810b1c8a873c4fca263
jamie109 <2356408976@qq.com>
2023-06-21 22:57:47 +0800

    init web

---
+++ src/Application.java
@@ -0,0 +1,20 @@
+class Dog{
+    int age;
+    
+
+}
+public class Application {
+    public static void main(String[] args) {
+        System.out.print("wjm");//打印
+        //变量小写字母开头，
+        final int $a = 1;
+        int $A = 2;
+
+        System.out.println($A);
+        System.out.println("Hello World!");
+        System.out.println("java");
+        for (int i = 0; i < 100; i++) {
+
+        }
+    }
+}
